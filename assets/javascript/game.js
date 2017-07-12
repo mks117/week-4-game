5fe277bcc290d5e16b5f2f9c532933fbf9f0f34c
Max S <mschmell@uci.edu>
2017-07-12 00:01:19 -0700

    submit

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,1 +1,62 @@
 console.log("js is working")
+
+var matchScore = 0;
+var wins = 0;
+var losses = 0;
+var total = 0;
+var gemTotals = [0,0,0,0];
+
+var gemScoreReveal = [false, false, false, false];
+
+
+matchScore = getRandomInt(19, 120);
+
+console.log(matchScore);
+
+for (var i = 0; i < gemTotals.length; i++) {
+	gemTotals[i] = getRandomInt(1, 12);
+}
+
+console.log(gemTotals)
+
+$(document).ready(function(){
+	$("#gem00").click(function() {
+		total += gemTotals[0];
+		console.log("gem00 " + total);
+	})
+
+	$("#gem01").click(function() {
+		total += gemTotals[1];
+		console.log("gem01 " + total);
+	})
+
+	$("#gem02").click(function() {
+		total += gemTotals[2];
+		console.log("gem02 " + total);
+	})
+
+	$("#gem03").click(function() {
+		total += gemTotals[3];
+		console.log("gem03 " + total);
+	})
+
+});
+
+function checkState() {
+	if(total === matchScore) {
+		wins++;
+		$("#player-wins").html(wins)
+	}
+
+	else if(total > matchScore) {
+		losses++;
+		$("#player-losses").html(losses)		
+	}	
+}
+
+
+function getRandomInt(min, max) {
+	min = Math.ceil(min);
+	max = Math.floor(max);
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
